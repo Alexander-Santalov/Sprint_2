9b02578fd0b3a235b42d71fbc16728dde75ce618
Alexander Santalov <bolid.santalov@yandex.ru>
2022-06-05 12:13:26 +0100

    Изменил мок в Cat

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -9,11 +9,10 @@ import static org.junit.Assert.*;
 
 public class CatTest extends TestBase {
     @Mock
-    Predator predator;
+    Feline feline;
 
     @Test
     public void getCatSoundTest() {
-        Feline feline = new Feline();
         Cat cat = new Cat(feline);
         String actualSound = cat.getSound();
         assertEquals("Мяу", actualSound);
@@ -21,10 +20,9 @@ public class CatTest extends TestBase {
 
     @Test
     public void getCatFoodTest() throws Exception {
-        Feline feline = new Feline();
         Cat cat = new Cat(feline);
         List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
-        Mockito.when(predator.eatMeat()).thenReturn(expectedFood);
+        Mockito.when(feline.eatMeat()).thenReturn(expectedFood);
         List<String> actualEatMeat = cat.getFood();
         assertEquals(expectedFood, actualEatMeat);
     }
